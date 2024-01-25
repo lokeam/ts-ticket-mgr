1663e559758865c10b1e7aa7433d3198a84e4b61
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-25 17:29:06 -0500

    feat: install typeorm + setup database config

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,19 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { DataSource } from "typeorm";
 
 const app:Express = express();
 dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: process.env.POSTGRE_USER,
+  password: process.env.POSTGRE_PASSWORD,
+  database: process.env.POSTGRE_DB,
+  synchronize: true,
+});
 
 const port = process.env.PORT;
 
@@ -10,4 +21,10 @@ app.get("/", (request:Request, response:Response) => {
   response.send("Hello from Express Server");
 });
 
-app.listen(port);
+AppDataSource.initialize().then(() => {
+
+  app.listen(port);
+  console.log('Data src initialized');
+}).catch((error) => {
+  console.error('Error during Data src initialization: ', error);
+});
