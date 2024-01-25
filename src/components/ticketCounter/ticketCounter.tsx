12967962762a73cf3101681c56931ddfcb658150
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-25 12:29:06 -0500

    style: add Material UI styling to Ticket Counter

--- src/components/ticketCounter/ticketCounter.tsx
+++ src/components/ticketCounter/ticketCounter.tsx
@@ -10,10 +10,27 @@ export const TicketCounter:FC = (): ReactElement => {
         justifyContent="center"
         alignItems="center"
       >
-        <Avatar>
-          <Typography>10</Typography>
+        <Avatar
+          sx={{
+            backgroundColor: 'transparent',
+            border: '5px solid',
+            width: '96px',
+            height: '96px',
+            marginBottom: '16px',
+            borderColor: 'warning.light',
+          }}
+        >
+          <Typography
+            color="#ffffff"
+            variant="h4"
+          >10</Typography>
         </Avatar>
-        <Typography>Subtitle here</Typography>
+        <Typography
+          color="#ffffff"
+          fontWeight="bold"
+          fontSize="20px"
+          variant="h5"
+        >Subtitle here</Typography>
       </Box>
     </>
   );
