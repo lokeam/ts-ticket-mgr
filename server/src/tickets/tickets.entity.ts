97bf4323af6e580bac8c7502470340df6f663dca
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-26 22:52:18 -0500

    feat: complete Ticket.entity for datasource

--- server/src/tickets/tickets.entity.ts
+++ server/src/tickets/tickets.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn
+} from "typeorm";
+
+import { Priority } from '../enums/Priority';
+import { Status } from "../enums/Status";
 
 // Note: Entity in TypeORM is always a class of properties
 // (all table cols as properties)
@@ -7,4 +14,34 @@ import { Entity, PrimaryGeneratedColumn } from "typeorm";
 export class Ticket {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({
+    type: 'text',
+  })
+  title: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  date: string;
+
+  @Column({
+    type: 'text',
+  })
+  description: string;
+
+  @Column({
+    type: 'enum',
+    enum: Priority,
+    default: Priority.normal,
+  })
+  priority: Priority;
+
+  @Column({
+    type: 'enum',
+    enum: Status,
+    default: Status.todo,
+  })
+  status: Status;
 }
