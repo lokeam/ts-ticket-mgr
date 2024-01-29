37edb412478c13351e322f8f132a3cfa265e137d
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-29 11:36:34 -0500

    feat: add sendApiRequest helper method

--- client/src/utils/sentApiRequest.ts
+++ client/src/utils/sentApiRequest.ts
@@ -1,15 +1,36 @@
 type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
-// return correct request helper fn
+function returnCorrectRequest(
+  method: Method,
+  data: unknown = {},
 
-export const sendApiRequest = async (
+):RequestInit {
+  if (method === 'GET') {
+    return {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    };
+  }
+
+  return {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  };
+};
+
+export async function sendApiRequest<T> (
   url: string,
   method: Method,
   data: unknown = {},
-): Promise<any> => {
+): Promise<T> {
   const response = await fetch(
     url,
-    // object different based on method type
+    returnCorrectRequest(method, data)
   );
 
   if (!response.ok) {
@@ -17,5 +38,5 @@ export const sendApiRequest = async (
     throw new Error(message);
   }
 
-  return await response.json();
+  return await response.json() as Promise<T>;
 };
